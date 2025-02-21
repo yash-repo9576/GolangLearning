535d44dea6ba72632604aad3242a55b459b80163
yash-repo9576 <yashrajput9576@gmail>
2025-02-21 17:25:32 +0530

    POST API using Gin

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GolangLearning
-Golang Learning Repository - ToDo List
+Golang Learning Repository - CRUD APP BookStore
+
+1. Get all Books - GET:
+```bash
+$ curl localhost:8080/books
+```
+
+2. Create a Book - POST:
+```bash
+$ curl localhost:8080/create-book --include --header "Content-Type: application/json" -d @body.json --request "POST"
+```
--- main.go
+++ main.go
@@ -23,8 +23,20 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook.Title)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/create-book", createBook)
 	router.Run("localhost:8080")
 }
